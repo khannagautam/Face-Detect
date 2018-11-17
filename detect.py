42b1f39090b5478032638804073587de2bd27948
Gautam Khanna <khannagautam96@gmail.com>
2018-11-17 14:20:36 +0530

    Added code

---
+++ detect.py
@@ -0,0 +1,52 @@
+import requests
+import matplotlib.pyplot as plt
+from PIL import Image
+from matplotlib import patches
+from io import BytesIO
+
+# Replace <Subscription Key> with your valid subscription key.
+subscription_key = "f264a424fd2f430c99cad98d1c825437"
+assert subscription_key
+
+# You must use the same region in your REST call as you used to get your
+# subscription keys. For example, if you got your subscription keys from
+# westus, replace "westcentralus" in the URI below with "westus".
+#
+# Free trial subscription keys are generated in the westcentralus region.
+# If you use a free trial subscription key, you shouldn't need to change
+# this region.
+face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+
+# Set image_url to the URL of an image that you want to analyze.
+image_url = 'https://avatars2.githubusercontent.com/u/28821763?s=400&v=4'
+
+headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender'
+}
+data = {'url': image_url}
+response = requests.post(face_api_url, params=params, headers=headers, json=data)
+faces = response.json()
+#print(faces)
+#print('json done')
+print('Image link')
+print(image_url)
+print(faces)
+# Display the original image and overlay it with the face information.
+image = Image.open(BytesIO(requests.get(image_url).content))
+plt.figure(figsize=(8, 8))
+ax = plt.imshow(image, alpha=0.6)
+for face in faces:
+    fr = face["faceRectangle"]
+    fa = face["faceAttributes"]
+    origin = (fr["left"], fr["top"])
+    p = patches.Rectangle(
+        origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
+    ax.axes.add_patch(p)
+    plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]),
+             fontsize=20, weight="bold", va="bottom")
+_ = plt.axis("off")
+
+
